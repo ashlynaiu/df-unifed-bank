9a4bbdda793c8107ad910dda69db181dc952ab2e
ashlynaiu <ashlyn.aiu@gmail.com>
2018-09-17 10:13:52 -0700

    add slds react package

--- src/Mobile.js
+++ src/Mobile.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './styles/mobile.css';
+import { Button } from '@salesforce/design-system-react';
 class Mobile extends Component {
   render() {
     return (
         <div className="mobile-frame">
             <div className="mobileContainer">
                 <div className="mobileContainer-inner">
-
+                <Button label="Hello Button" />
                 </div>
             </div>
         </div>
